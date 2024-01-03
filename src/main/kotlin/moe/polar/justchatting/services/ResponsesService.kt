46de5debe24ec7aca3de4ba240646ea7ee291770
httpolar <http@polar.moe>
2024-01-04 03:51:10 +0400

    Created function to quickly throw status code responses

---
+++ src/main/kotlin/moe/polar/justchatting/services/ResponsesService.kt
@@ -0,0 +1,16 @@
+package moe.polar.justchatting.services
+
+import io.ktor.http.HttpStatusCode
+import moe.polar.justchatting.errors.responses.StatusCodeError
+
+fun unauthorized(message: String = "Unauthorized"): Nothing {
+    throw StatusCodeError(HttpStatusCode.Unauthorized, message)
+}
+
+fun badRequest(message: String = "Bad Request"): Nothing {
+    throw StatusCodeError(HttpStatusCode.BadRequest, message)
+}
+
+fun internalServerError(message: String = "Internal Server Error"): Nothing {
+    throw StatusCodeError(HttpStatusCode.InternalServerError, message)
+}
