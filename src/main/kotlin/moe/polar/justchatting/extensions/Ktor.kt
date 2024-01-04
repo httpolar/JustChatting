fbfe42838846a27111fae113c35e32bb5cb45a28
httpolar <http@polar.moe>
2024-01-04 18:26:35 +0400

    Updated ApplicationCall.requireUser()

--- src/main/kotlin/moe/polar/justchatting/extensions/Ktor.kt
+++ src/main/kotlin/moe/polar/justchatting/extensions/Ktor.kt
@@ -4,11 +4,12 @@ import io.ktor.server.application.ApplicationCall
 import io.ktor.server.auth.principal
 import io.ktor.server.auth.Principal
 import moe.polar.justchatting.entities.dao.User
-import moe.polar.justchatting.principals.UserPrincipal
+import moe.polar.justchatting.entities.dao.getUser
+import moe.polar.justchatting.principals.UserIdPrincipal
 import moe.polar.justchatting.services.unauthorized
 
 inline fun <reified T : Principal> ApplicationCall.requirePrincipal(): T =
     principal<T>() ?: unauthorized()
 
-fun ApplicationCall.requireUser(): User =
-    requirePrincipal<UserPrincipal>().user
+suspend fun ApplicationCall.requireUser(): User =
+    requirePrincipal<UserIdPrincipal>().id.getUser()
