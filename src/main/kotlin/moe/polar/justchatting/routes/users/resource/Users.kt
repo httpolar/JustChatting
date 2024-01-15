231d3f32b7752cdb154682d8e0f53d0bcb5c673d
httpolar <http@polar.moe>
2024-01-15 19:08:58 +0400

    Resource route of /users

--- src/main/kotlin/moe/polar/justchatting/routes/users/resource/Users.kt
+++ src/main/kotlin/moe/polar/justchatting/routes/users/resource/Users.kt
@@ -1,8 +1,18 @@
 package moe.polar.justchatting.routes.users.resource
 
 import io.ktor.resources.Resource
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 @Resource("/users")
-class UsersResource
+class UsersResource {
+    @Serializable
+    @Resource("@me")
+    class Me(val parent: UsersResource = UsersResource())
+
+    @Serializable
+    @Resource("{id}")
+    class Id(val parent: UsersResource = UsersResource(), val id: @Contextual UUID)
+}
